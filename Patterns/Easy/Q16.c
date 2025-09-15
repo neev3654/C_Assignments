579b90ae8d834b30c5de95ca7ffb0a8d1084908a
neev3654 <neev.patel.cg@gmail.com>
2025-09-15 10:33:34 +0530

    Made changes in the code

--- Patterns/Easy/Q16.c
+++ Patterns/Easy/Q16.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
-int main()
-{
+
+int main(){
+
     int n;
-    scanf("%d",&n);
-    char star = '*';
-    for (int i = 1; i <= n; i++)
-    {
+    printf("enter the value: ");
+    scanf("%d", &n);
+    
+    for(int i=1;i<=n; i++){
+        
+        for(int k=1;k<=n-i+1;k++){
+            printf("*");
+        }
+        
         printf("\n");
-        for (int j = if (condition)
-        {
-            /* code */
-        }
-        else if (condition)
-        {
-            /* code */
-        }
-        else
-        {
-            /* code */
-        }
-        ; j >=; j--)
-        {
-            printf("%c", star);
-        }
     }
+
+    return 0;
 }
